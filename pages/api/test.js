8b8dafee6ab84c440241ce19286cc814d0763243
Bajram Gerbeshi <bajramgerbeshi@MBPsomtorBajram.lan>
2021-01-21 19:21:00 +0100

    Changed env

--- pages/api/test.js
+++
@@ -1,4 +0,0 @@
-require('dotenv').config({path: '/Users/bajramgerbeshi/WebstormProjects/untitled4/newPortfolio/.env'})
-const firstname = process.env.TEST;
-const password = process.env.PW;
-console.log(firstname)
